9e9a30ebe44c91015bee7f6f40627bc18312b96f
seshairam11 <4602seshasairam@gmail.com>
2025-07-22 18:34:14 +0530

    api string

--- frontend/src/ApiOP/useFetch.js
+++ frontend/src/ApiOP/useFetch.js
@@ -4,7 +4,7 @@ const useFetch = () => {
   const [isLoadingApi, setisLoading] = useState(false);
   const [fetchError, setFetchError] = useState(null);
   const [apiKey, setApiKey] = useState(null);
-  const baseURI = "https://agricultare-instument-backend.netlify.app";
+  const baseURI = "https://agricultar-instumental-rent.onrender.com";
 
 
 
